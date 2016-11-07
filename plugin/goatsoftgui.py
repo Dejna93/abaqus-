27ff676f3fcab2e298a3f4037d75445239d57429
1Shalafi1 <Aratosek1@gmail.com>
2016-11-07 23:53:40 +0100

    Gui: * Added very first gui TODO: *removeit!!!

--- plugin/goatsoftgui.py
+++ plugin/goatsoftgui.py
@@ -6,7 +6,6 @@ FONT = ("Times New Roman", 8)
 
 
 class Capp(tk.Tk):
-
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
@@ -32,7 +31,7 @@ class Capp(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, ConfigPage):
+        for F in (StartPage, PageOne, MainPage):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -53,15 +52,13 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
         self.parent = parent
-        button1 = ttk.Button(self, text="OK", command=lambda: controller.show_frame("ConfigPage"))
+        button1 = ttk.Button(self, text="OK", command=lambda: controller.show_frame("MainPage"))
         button1.grid(row=1, column=1)
         button2 = ttk.Button(self, text="Quit", command=lambda: StartPage.quit(self))
         button2.grid(row=1, column=2)
 
 
-class ConfigPage(tk.Frame):
-    filename = ''
-
+class MainPage(tk.Frame):
     def __init__(self, parent , controller):
         tk.Frame.__init__(self, parent)
         self.parent = parent
@@ -77,34 +74,68 @@ class ConfigPage(tk.Frame):
         self.entry_1 = tk.Entry(labelFrame_1, bd=2, width=50)
         self.entry_1.pack(side=tk.LEFT, fill=tk.X, padx=20)
 
+########################################################################################################################
+
         labelFrame_2 = tk.LabelFrame(self, text="Operations", height=100, padx=10, pady=10)
         labelFrame_2.pack(fill="both", padx=5, pady=5)
 
-        label_2 = tk.Label(labelFrame_2, text="Check file")
-        label_2.pack(side=tk.LEFT, padx=20)
+        radiobutton_inc = tk.Radiobutton(labelFrame_2, padx=10, pady=10).grid(row=0, column=0)
+        radio_labels_inc = tk.Label(labelFrame_2, text=u"All elements", bg="white").grid(row=0, column=1)
 
-        btn_2 = tk.Button(labelFrame_2, text="Valid", command=lambda: self.fileoper.validate(self.filename))
-        btn_2.pack(side=tk.LEFT, padx=10)
+        radiobutton_inc_2 = tk.Radiobutton(labelFrame_2, padx=10, pady=10).grid(row=1, column=0)
+        radio_labels_inc_2 = tk.Label(labelFrame_2, text=u'Selected', bg="white").grid(row=1, column=1)
+        entry_inc_2 = tk.Entry(labelFrame_2, bd=2, width=4).grid(row=1, column=6)
+        entry_labels_inc_2 = tk.Label(labelFrame_2, text=u'Set max', bg="white").grid(row=1, column=5)
 
-        label_2 = tk.Label(labelFrame_2, text="Create STL")
-        label_2.pack(side=tk.LEFT, padx=10)
-        btn_3 = tk.Button(labelFrame_2, text="OK", command=lambda: self.check_filepath(controller))
-        btn_3.pack(side=tk.LEFT, padx=10)
+        radiobutton_inc_3 = tk.Radiobutton(labelFrame_2, padx=10, pady=10).grid(row=3, column=0)
+        radio_labels_inc_3 = tk.Label(labelFrame_2, text=u'Selected', bg="white").grid(row=3, column=1)
+        entry_inc_3 = tk.Entry(labelFrame_2, bd=2, width=4).grid(row=3, column=4)
+        entry_labels_inc_3 = tk.Label(labelFrame_2, text=u'Set max', bg="white").grid(row=3, column=5)
+        entry_inc_4 = tk.Entry(labelFrame_2, bd=2, width=4).grid(row=3, column=6)
+        entry_labels_inc_4 = tk.Label(labelFrame_2, text=u'Set max', bg="white").grid(row=3, column=5)
 
-        label_3 = tk.Label(labelFrame_2, text="Import Geometry")
-        label_3.pack(side=tk.LEFT, padx=10)
+########################################################################################################################
 
-        btn_4 = tk.Button(labelFrame_2, text="Import")
-        btn_4.pack(side=tk.LEFT, padx=10)
+        labelFrame_3 = tk.LabelFrame(self, text="Elements", height=100, padx=10, pady=10)
+        labelFrame_3.pack(fill="both", padx=5, pady=5)
 
+        radiobutton_inc = tk.Radiobutton(labelFrame_3, padx=10, pady=10).grid(row=0, column=0)
+        radio_labels_inc = tk.Label(labelFrame_3, text=u"All elements", bg="white").grid(row=0, column=1)
 
-    def askopenfile(self):
-        self.filename = tkFileDialog.askopenfilename(**self.file_opt)
+        radiobutton_inc_2 = tk.Radiobutton(labelFrame_3, padx=10, pady=10).grid(row=1, column=0)
+        radio_labels_inc_2 = tk.Label(labelFrame_3, text=u'Selected', bg="white").grid(row=1, column=1)
+        entry_inc_2 = tk.Entry(labelFrame_3, bd=2, width=4).grid(row=1, column=6)
+        entry_labels_inc_2 = tk.Label(labelFrame_3, text=u'Set max', bg="white").grid(row=1, column=5)
 
-        if self.filename:
-            print self.filename
-            self.set_entry(self.filename)
-            return open(self.filename, 'r')
+        radiobutton_inc_3 = tk.Radiobutton(labelFrame_3, padx=10, pady=10).grid(row=3, column=0)
+        radio_labels_inc_3 = tk.Label(labelFrame_3, text=u'Selected', bg="white").grid(row=3, column=1)
+        entry_inc_3 = tk.Entry(labelFrame_3, bd=2, width=4).grid(row=3, column=4)
+        entry_labels_inc_3 = tk.Label(labelFrame_3, text=u'Set max', bg="white").grid(row=3, column=5)
+        entry_inc_4 = tk.Entry(labelFrame_3, bd=2, width=4).grid(row=3, column=6)
+        entry_labels_inc_4 = tk.Label(labelFrame_3, text=u'Set max', bg="white").grid(row=3, column=5)
+
+########################################################################################################################
+
+        labelFrame_4 = tk.LabelFrame(self, text="Output request", height=100, padx=10, pady=10)
+        labelFrame_4.pack(fill="both", padx=5, pady=5)
+        radiobutton_out = tk.Radiobutton(labelFrame_4, padx=10, pady=10).grid(row=0, column=0)
+        radio_labels_inc_2 = tk.Label(labelFrame_4, text=u'2D', bg="white").grid(row=0, column=1)
+
+        radiobutton_out = tk.Radiobutton(labelFrame_4, padx=10, pady=10).grid(row=0, column=2)
+        radio_labels_inc_2 = tk.Label(labelFrame_4, text=u'3D', bg="white").grid(row=0, column=3)
+        radio_labels_inc_2 = tk.Label(labelFrame_4, text=u'3D', bg="white").grid(row=0, column=3)
+        names2D = ['S', 'PEEQ', 'EVOL', 'SDV121', 'U']
+        names3D = ['S', 'T', 'D']
+
+        list2d = tk.Listbox(labelFrame_4)
+        for name in names2D:
+            list2d.insert(tk.END, name)
+        list2d.grid(row=1, column=0)
+
+        list3d = tk.Listbox(labelFrame_4)
+        for name in names3D:
+            list3d.insert(tk.END, name)
+        list3d.grid(row=1, column=2)
 
     def set_entry(self, text):
         self.entry_1.delete(0, tk.END)
@@ -139,3 +170,6 @@ def run_gui():
     app = Capp()
     app.minsize(width=400,height=400)
     app.mainloop()
+
+if __name__ == '__main__':
+    run_gui()
