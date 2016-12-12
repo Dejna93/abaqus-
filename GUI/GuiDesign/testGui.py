98b0604cdf6d763539482f81abc6d6478c5d718e
khemzacz <Konrad622@gmail.com>
2016-12-12 15:18:51 +0100

    - Gui update.

--- GUI/GuiDesign/testGui.py
+++ GUI/GuiDesign/testGui.py
@@ -6,7 +6,7 @@ import ttk
 
 #execfile("abaqusCommands.py")
 import abaqusCommands
-import actualAbaqusCommands
+# import actualAbaqusCommands
 
 import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -16,8 +16,8 @@ def testFunction():
     class GuiApplication:
         def __init__(self):
             self.master = Tk()
-            self.abaqus = actualAbaqusCommands.ActualAbaqusCommands()
-            # self.abaqus = abaqusCommands.AbaqusCommands()
+            # self.abaqus = actualAbaqusCommands.ActualAbaqusCommands()
+            self.abaqus = abaqusCommands.AbaqusCommands()
 
             self.specimenWidth = StringVar()
             self.vfaz = (self.master.register(self.specimen_dimension_validate), '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
@@ -34,17 +34,19 @@ def testFunction():
             self.sphericalIndenterImage = PhotoImage(file=dir_path+"/"+"PrototypSpherical.ppm")
             self.vickersIndenterImage = PhotoImage(file=dir_path+"/"+"PrototypVickers.ppm")
             self.berkovichIndenterImage = PhotoImage(file=dir_path+"/"+"PrototypBerkovich.ppm")
+            self.materialLabel = ttk.Label(self.master, text="Select the material of specimen")
+            self.materialLabel.pack()
             Radiobutton(self.master, text="steel", variable=self.v, value=1, command=self.steel).pack(anchor=W)
             Radiobutton(self.master, text="titanium", variable=self.v, value=2, command=self.titanium).pack(anchor=W)
 
             self.frame = Frame(self.master)
             self.frame.pack()
 
-            self.indenterLabel = ttk.Label(self.frame, text="Please select one of the indenters available below:")
+            self.indenterLabel = ttk.Label(self.master, text="Please select one of the indenters available below:")
             self.indenterLabel.pack()
             self.indenters = ('spherical', 'Vickers', 'Berkovich')
-            self.indenterComboBox = ttk.Combobox(self.frame, values=self.indenters, textvariable=self.indenters)
-            self.indenterImageLabel = ttk.Label(self.frame, image=self.sphericalIndenterImage)
+            self.indenterComboBox = ttk.Combobox(self.master, values=self.indenters, textvariable=self.indenters)
+            self.indenterImageLabel = ttk.Label(self.master, image=self.sphericalIndenterImage)
             self.indenterComboBox.current(newindex=0)
             self.indenterComboBox.bind("<<ComboboxSelected>>", self.update_gui_on_selected_indenter)
             self.indenterComboBox.pack()
@@ -58,11 +60,11 @@ def testFunction():
             self.indenterImageLabel.image = self.sphericalIndenterImage
             self.indenterImageLabel.pack()
 
-            self.specimenWidthLabel = ttk.Label(self.frame, text="Specimen width:")
+            self.specimenWidthLabel = ttk.Label(self.master, text="Specimen width:")
             self.specimenWidthEntry = ttk.Entry(self.master, validate='key', validatecommand=self.vfaz, textvariable=self.specimenWidth)
-            self.specimenLengthLabel = ttk.Label(self.frame, text="Specimen length:")
+            self.specimenLengthLabel = ttk.Label(self.master, text="Specimen length:")
             self.specimenLengthEntry = ttk.Entry(self.master, validate='key', validatecommand=self.vfaz, textvariable=self.specimenLength)
-            self.specimenHeightLabel = ttk.Label(self.frame, text="Specimen height:")
+            self.specimenHeightLabel = ttk.Label(self.master, text="Specimen height:")
             self.specimenHeightEntry = ttk.Entry(self.master, validate='key', validatecommand=self.vfaz, textvariable=self.specimenHeight)
             self.specimenWidthLabel.pack()
             self.specimenWidthEntry.pack()
@@ -71,7 +73,7 @@ def testFunction():
             self.specimenHeightLabel.pack()
             self.specimenHeightEntry.pack()
 
-            self.button = Button(self.frame, text="done", command=self.close_window)
+            self.button = Button(self.master, text="done", command=self.close_window)
             self.button.pack()
 
 
